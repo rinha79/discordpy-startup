4992aecb91b6c1f0895f22fe0e0db058e2beb212
rinha79 <71050971+rinha79@users.noreply.github.com>
2020-09-20 14:01:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,4 @@
+
 import discord
 from discord.ext import commands
 import os
@@ -9,7 +10,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def on_ready():
     channel = bot.get_channel(696922604660850740)
-    await channel.send("何故でしょう")
+    await channel.send("起動")
         
 @bot.event
 async def on_command_error(ctx, error):
@@ -20,11 +21,29 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+    
+@bot.command()
+async def joined(ctx, *, member: discord.Member):
+    await ctx.send('{0} joined on {0.joined_at}'.format(member))
  
+@bot.command()
+async def is_owner(ctx):
+    return ctx.author.id == 567999794879135745
+
+@bot.command(name='eval')
+@commands.check(is_owner)
+async def _eval(ctx, *, code):
+    """A bad example of an eval command"""
+    await ctx.send(eval(code))
 @bot.event
 async def on_message(message):
     if message.channel.id == 696922604660850740:
         is_bot = " [BOT]"if message.author.bot else ""
         print("{0.author}{1}\n{0.content}".format(message,is_bot))
+    await bot.process_commands(message)
 
 bot.run(token)
