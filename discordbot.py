f77975584e299ef1b11057cefe88f3bb73fe6313
rinha79 <71050971+rinha79@users.noreply.github.com>
2020-09-20 13:44:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,20 +25,6 @@ async def ping(ctx):
 async def test(ctx, arg):
     await ctx.send(arg)
     
-@bot.command()
-async def joined(ctx, *, member: discord.Member):
-    await ctx.send('{0} joined on {0.joined_at}'.format(member))
- 
-@bot.command()
-async def is_owner(ctx):
-    return ctx.author.id == 567999794879135745
-
-@bot.command(name='eval')
-@commands.check(is_owner)
-async def _eval(ctx, *, code):
-    """A bad example of an eval command"""
-    await ctx.send(eval(code))
-    
 @bot.event
 async def on_message(message):
     if message.channel.id == 696922604660850740:
