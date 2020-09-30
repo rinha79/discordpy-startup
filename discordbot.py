cf836525b7b475d729b6d1efcf0887e45ced7b1f
rinha79 <71050971+rinha79@users.noreply.github.com>
2020-09-30 15:13:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,7 +10,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def on_ready():
     channel = bot.get_channel(696922604660850740)
-    await channel.send(":OwO:")   
+    await channel.send("<:OwO:639027637812985866>")   
         
 @bot.event
 async def on_command_error(ctx, error):
