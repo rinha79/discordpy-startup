0403532b921ac7a4d928f754488dc8522b43553a
rinha79 <71050971+rinha79@users.noreply.github.com>
2020-11-18 09:56:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,7 +10,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def on_ready():
     channel = bot.get_channel(696922604660850740)
-    await channel.send("なんだぽまえ")   
+    await channel.send("🤣")   
         
 @bot.event
 async def on_command_error(ctx, error):
